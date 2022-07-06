f79db4aef3c8c652b5c5d245dfec8ed5d6b56cc1
thompscott <tho.sco.lisd@gmail.com>
2022-07-06 10:11:44 -0500

    Index App render set up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { App } from "./components";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world!</h1>);
+root.render(
+<BrowserRouter>
+<App/>
+</BrowserRouter>);
 
 
